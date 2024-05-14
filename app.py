8d62b56ce63f55e049c48d182110897977b9af3f
BrunoCTmb <zazaduza10@gmail.com>
2024-05-14 17:46:34 -0300

    atualizar usuarios apenas com notas

--- app.py
+++ app.py
@@ -31,14 +31,26 @@ class Database:
                               VALUES (?, ?, ?, ?); ''', (name, email, rgm, password))
             self.conn.commit()
         except:
-            print("ERRO DB!")
+            print("ERRO EM INSERIR!")
+
+    def updateGrade(self, id, a2, a1, atividades, nota_final):
+        self.cursor.execute(
+            '''UPDATE users SET nota_a2 = ?, nota_a1 = ?, nota_atividades = ?, nota_final = ? WHERE id = ?''',
+            (a2, a1, atividades, nota_final, id))
+        self.conn.commit()
+        print("salvou")
+
+    def getStudentById(self, id=1):
+        self.cursor.execute('''SELECT * FROM users WHERE id = ?''', (id))
+        return self.cursor.fetchone()
 
 
-    def getUsers(self):
+    def getStudents(self):
         return self.conn.execute("SELECT * FROM users")
 
     def closeConnection(self):
         self.cursor.close()
+
 
 class Application:
     def __init__(self, master):
@@ -53,22 +65,25 @@ class Application:
         self.root.geometry("500x500")
 
     def mainPage(self):
-        studentRegister = Button(self.root, text="Registrar um aluno", command=self.registerWidgets)
+        studentRegister = Button(self.root, text="Registrar um aluno", command=self.registerStudents)
         studentRegister.pack()
 
-        showStudents = Button(self.root, text="Mostrar todos alunos", command=self.showUsers)
+        showStudents = Button(self.root, text="Mostrar todos alunos", command=self.showStudents)
         showStudents.pack()
 
-        editStudents = Button(self.root, text="Editar nota do aluno")
+        editStudents = Button(self.root, text="Editar nota do aluno", command=self.findStudent)
         editStudents.pack()
 
     def topRoot(self):
         self.root2 = Toplevel()
         # self.root2.destroy()
-        # self.root2.geometry("800x500")
+        self.root2.geometry("1800x500")
 
-    def registerWidgets(self):
+    def registerStudents(self):
         self.topRoot()
+          
+        title = Label(self.root2, text="REGISTRAR ALUNO")
+        title.pack()
 
         nameLabel = Label(self.root2, text="Nome do aluno: ")
         nameLabel.pack()
@@ -95,14 +110,14 @@ class Application:
         self.confirm = Entry(self.root2)
         self.confirm.pack()
 
-        submit = Button(self.root2, text="Enviar", command=self.saveUser)
+        submit = Button(self.root2, text="Enviar", command=self.saveStudent)
         submit.pack()
 
-    def saveUser(self):
+    def saveStudent(self):
         print(self.name.get(), self.email.get(), self.rgm.get(), self.password.get())
         self.db.insert(self.name.get(), self.email.get(), self.rgm.get(), self.password.get())
 
-    def showUsers(self):
+    def showStudents(self):
         self.topRoot()
 
         table = ttk.Treeview(self.root2, columns=("ID", "Nome", "Email", "RGM", "A2", "A1", "Atividades", "Nota final"))
@@ -115,7 +130,7 @@ class Application:
         table.heading("#6", text="Atividades")
         table.heading("#7", text="Nota final")
 
-        users = self.db.getUsers()
+        users = self.db.getStudents()
 
         for user in users:
             print(user)
@@ -123,6 +138,46 @@ class Application:
 
         table.pack(expand=True, fill="both")
 
+    def findStudent(self):
+        self.topRoot()
+
+        findId = Label(self.root2, text="Digite o id do aluno: ")
+        findId.pack()
+        findIdEntry = Entry(self.root2)
+        findIdEntry.pack()
+        findIdButton = Button(self.root2, text="Encontrar", command=lambda: self.editStudent(findIdEntry.get()))
+        findIdButton.pack()
+
+
+    def editStudent(self, id):
+        aluno = self.db.getStudentById(id)
+
+        title = Label(self.root2, text=f"EDITANDO NOTA DE: {aluno[1]}")
+        title.pack()
+
+        a2Label = Label(self.root2, text="Nota A2: ")
+        a2Label.pack()
+        a2 = Entry(self.root2)
+        a2.pack()
+
+        a1Label = Label(self.root2, text="Nota A1: ")
+        a1Label.pack()
+        a1 = Entry(self.root2)
+        a1.pack()
+
+        atividadesLabel = Label(self.root2, text="Nota de Atividades: ")
+        atividadesLabel.pack()
+        atividades = Entry(self.root2)
+        atividades.pack()
+
+        finalLabel = Label(self.root2, text="Nota Final: ")
+        finalLabel.pack()
+        final = Entry(self.root2)
+        final.pack()
+
+        submit = Button(self.root2, text="ATUALIZAR NOTA",
+                        command=lambda: self.db.updateGrade(id, a2.get(), a1.get(), atividades.get(), final.get()))
+        submit.pack()
 
 
 def app():
